85be44cd8070781c0b408eabbc77c4fee89ea260
Fernando Lucas de Castro Cavalcante <160240132+DaviSF09@users.noreply.github.com>
2024-04-19 10:29:24 -0300

    jajav

--- calcu_média.js
+++ calcu_média.js
@@ -1,13 +1,15 @@
 const nota1 = 9.4
 const nota2 = 7.3
 const nota3 = 5.2
-const media = (nota1 + nota2 + nota3)/3
+function media (a,b,c){
+    return (a+b+c)/3
+}
 
 console.log(`Notas dos Alunos: ${nota1};    ${nota2};   ${nota3};`)
-console.log(`Média das notas: ${media}`)
-if (media >= 6){
-    console.log(`Parabéns, a média da turma (${media}) é suficiente para a aprovação!!`)
+console.log(`Média das notas: ${media(nota1,nota2,nota3)}`)
+if (media(nota1,nota2,nota3) >= 6){
+    console.log(`Parabéns, a média da turma ${media(nota1,nota2,nota3)} é suficiente para a aprovação!!`)
 }
 else{
-    console.log(`Infelizmente, a média da turma (${media} não é suficiente para a aprovação)`)
+    console.log(`Infelizmente, a média da turma ${media(nota1,nota2,nota3)} não é suficiente para a aprovação`)
 }
---
+++ for_let.js
@@ -0,0 +1,4 @@
+for (let amor = 0;amor<100001;amor++){
+    console.log(`Meu amor por você é igual a ${amor} a velocidade de expansão do universo`) 
+}
+console.log("TE AMO <3")
