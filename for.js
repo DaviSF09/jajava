60b4a5397f201f37949af92da80e819d192e3a25
cristhyne007 <160240132+DaviSF09@users.noreply.github.com>
2024-04-26 10:25:41 -0300

    tuts

---
+++ for.js
@@ -0,0 +1,22 @@
+let blz = 0
+const volei = [
+    {
+        nome: "bene",
+        idade: 18,
+        altura: 1.70},
+    {
+        nome: "davi",
+        idade: 15,
+        altura: 1.71},
+    {
+        nome: "pedro",
+        idade: 16,
+        altura: 1.75},
+    {
+        nome: "andré",
+        idade: 17,
+        altura: 1.67},
+]
+for (let blz = 0; blz<volei.length; blz++){
+    console.log(volei[blz].nome)
+}
